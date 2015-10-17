686c605735adce8258590fc178b93d84eb4e0a08
Barry Skidmore <bskidmore@alertlogic.com>
2015-10-17 12:32:33 -0500

    Refactor sources to use a base API library for all queries

---
+++ utilities/api.js
@@ -0,0 +1,117 @@
+var https   = require('https'),
+    config  = require('../config.js'),
+    api     = require('./api.js'),
+    version = 'v1',
+    client  = 'alert-logic-nodejs-v1',
+    /*
+     * Fetches any single item from a service
+     */
+    getOne = function(token, service, endpoint, id, callback) {
+        "use strict";
+        var accountId = getAccountId(token),
+            options   = {
+                hostname: config.api_url,
+                port: 443,
+                path: '/' + service + '/' + version + '/' + config.accountId + '/' + endpoint +'/' + id,
+                method: 'GET',
+                headers: {
+                    "user-agent": client,
+                    "x-aims-auth-token": token
+                }
+            },
+            req       = https.request(options, function(res){
+                res.setEncoding('utf-8');
+                var responseString = '';
+
+                res.on('data', function(data) {
+                    responseString += data;
+                });
+
+                res.on('end', function() {
+                    if (res.statusCode === 200) {
+                        var json = JSON.parse(responseString);
+                        callback("SUCCESS", json);
+                    } else {
+                        callback("FAILED", res.statusCode);
+                    }
+                });
+            });
+
+        req.on('error', function(e) {
+            console.log('Call to ' + service + ' for ' + endpoint + ' failed because:\n' + e.message);
+            callback("FAILED", e.message);
+        });
+        req.end();
+    },
+    /*
+     * Fetches any set of items from a service
+     */
+    getAll = function(token, service, endpoint, query, callback) {
+        "use strict";
+
+        var accountId = getAccountId(token),
+            result    = parseQueryString(query),
+            options   = {
+                hostname: config.api_url,
+                port: 443,
+                path: '/' + service + '/' + version + '/' + config.accountId + '/' + endpoint + result,
+                method: 'GET',
+                headers: {
+                    "user-agent": client,
+                    "x-aims-auth-token": token
+                }
+            },
+            req       = https.request(options, function(res){
+                res.setEncoding('utf-8');
+                var responseString = '';
+
+                res.on('data', function(data) {
+                    responseString += data;
+                });
+
+                res.on('end', function() {
+                    if (res.statusCode === 200) {
+                        var json = JSON.parse(responseString);
+                        callback("SUCCESS", json);
+                    } else {
+                        callback("FAILED", res.statusCode);
+                    }
+                });
+            });
+
+        req.on('error', function(e) {
+            console.log('Call to ' + service + ' for ' + endpoint + ' failed because:\n' + e.message);
+            callback("FAILED", e.message);
+        });
+        req.end();
+    };
+
+/*
+ * Parses a Cloud Insight token to find the account id
+ */
+function getAccountId(token) {
+    if ( !config.hasOwnProperty('accountId') ||
+         config.accountId === "" ) {
+        config.accountId = JSON.parse(new Buffer(token.split(".")[1], 'base64')).account;
+    }
+    return config.accountId;
+}
+
+/*
+ * Turns an object of key value pairs into an http query string
+ */
+function parseQueryString(query) {
+    var result = '';
+    for (var index in query) {
+        result = (result === '') ? index + '=' + query[index] : '&' + index + '=' + query[index];
+    }
+    return (result === '') ? '' : '?' + result;
+}
+
+/*
+ * Export available public methods to external clients
+ */
+module.exports = {
+    "getOne": getOne,
+    "getAll": getAll
+};
--- utilities/sources.js
+++ utilities/sources.js
@@ -1,87 +1,14 @@
-var config     = require('../config.js'),
+var api        = require('./api.js'),
     getSources = function(token, callback) {
     "use strict";
-
-    if ( !config.hasOwnProperty('accountId') ||
-         config.accountId === "" ) {
-        config.accountId = JSON.parse(new Buffer(token.split(".")[1], 'base64')).account;
-    }
-
-    var https   = require('https'),
-        options = {
-            hostname: config.api_url,
-            port: 443,
-            path: '/sources/v1/' + config.accountId + '/sources?source.type=environment',
-            method: 'GET',
-            headers: {
-                "x-aims-auth-token": token
-            }
-        },
-        req     = https.request(options, function(res){
-            res.setEncoding('utf-8');
-            var responseString = '';
-
-            res.on('data', function(data) {
-                responseString += data;
-            });
-
-            res.on('end', function() {
-                if (res.statusCode === 200) {
-                    var json = JSON.parse(responseString);
-                    callback("SUCCESS", json);
-                } else {
-                    callback("FAILED", res.statusCode);
-                }
-            });
-        });
-
-    req.on('error', function(e) {
-        console.log('problem with request: ' + e.message);
-        callback("FAILED", e.message);
-    });
-    req.end();
+    var query = {
+        "source.type": "environment"
+    };
+    api.getAll(token, 'sources', 'sources', query, callback);
 },
     getCredential = function(token, id, callback) {
     "use strict";
-
-    if ( !config.hasOwnProperty('accountId') ||
-         config.accountId === "" ) {
-        config.accountId = JSON.parse(new Buffer(token.split(".")[1], 'base64')).account;
-    }
-
-    var https   = require('https'),
-        options = {
-            hostname: config.api_url,
-            port: 443,
-            path: '/sources/v1/' + config.accountId + '/credentials/' + id,
-            method: 'GET',
-            headers: {
-                "x-aims-auth-token": token
-            }
-        },
-        req     = https.request(options, function(res){
-            res.setEncoding('utf-8');
-            var responseString = '';
-
-            res.on('data', function(data) {
-                responseString += data;
-            });
-
-            res.on('end', function() {
-                if (res.statusCode === 200) {
-                    var json = JSON.parse(responseString);
-                    callback("SUCCESS", json);
-                } else {
-                    callback("FAILED", res.statusCode);
-                }
-            });
-        });
-
-    req.on('error', function(e) {
-        console.log('problem with request: ' + e.message);
-        callback("FAILED", e.message);
-    });
-    req.end();
+    api.getOne(token, 'sources', 'credentials', id, callback);
 };
 
 module.exports = {
